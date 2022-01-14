db291e6693c4146e54395dc2a6349ea98effeabe
VinicinBraga <vinicius@bragamatta.com.br>
2022-01-14 16:09:40 -0300

    Form Structure

---
+++ src/components/Form.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+function Form() {
+  const [page, setPage] = useState(0);
+
+  const FormTitles = ["Sign Up", "Personal Info", "Other"];
+
+  const PageDisplay = () => {};
+
+  return (
+    <div className="form">
+      <div className="progressbar">
+        <div
+          style={{
+            width: page === 0 ? "33.3%" : page === 1 ? "66.6%" : "100%",
+          }}
+        ></div>
+      </div>
+      <div className="form-container">
+        <div className="header">
+          <h1>{FormTitles[page]}</h1>
+        </div>
+        <div className="body">{PageDisplay()}</div>
+        <div className="footer">
+          <button
+            disabled={page === 0}
+            onClick={() => {
+              setPage((currPage) => currPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button
+            onClick={() => {
+              if (page === FormTitles.length - 1) {
+                alert("FORM SUBMITTED");
+              } else {
+                setPage((currPage) => currPage + 1);
+              }
+            }}
+          >
+            {page === FormTitles.length - 1 ? "Submit" : "Next"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
